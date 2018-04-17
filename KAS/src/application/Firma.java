e5b2b67b1456e0ff9fbd7eefd0b384fa9c989cd0
Administrator <Administrator@10.24.64.240>
2018-04-17 12:34:14 +0200

    Renamed "gui" package.

--- KAS/src/application/Firma.java
+++ KAS/src/application/Firma.java
@@ -1,37 +1,34 @@
 package application;
 
 public class Firma {
-	private String navn;
-	private String telefonNr;
-	private boolean betalerForLedsager;
-	private void namekk() {
-		
-	}
+    private String navn;
+    private String telefonNr;
+    private boolean betalerForLedsager;
 
-	public Firma(String navn, String telefonNr, boolean betalerForLedsager) {
-		this.navn = navn;
-		this.telefonNr = telefonNr;
-		this.betalerForLedsager = betalerForLedsager;
-	}
+    public Firma(String navn, String telefonNr, boolean betalerForLedsager) {
+        this.navn = navn;
+        this.telefonNr = telefonNr;
+        this.betalerForLedsager = betalerForLedsager;
+    }
 
-	public String getNavn() {
-		return navn;
-	}
+    public String getNavn() {
+        return navn;
+    }
 
-	public void setNavn(String navn) {
-		this.navn = navn;
-	}
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
 
-	public String getTelefonNr() {
-		return telefonNr;
-	}
+    public String getTelefonNr() {
+        return telefonNr;
+    }
 
-	public void setTelefonNr(String telefonNr) {
-		this.telefonNr = telefonNr;
-	}
+    public void setTelefonNr(String telefonNr) {
+        this.telefonNr = telefonNr;
+    }
 
-	public boolean betalerForLedsager() {
-		return betalerForLedsager;
-	}
+    public boolean betalerForLedsager() {
+        return betalerForLedsager;
+    }
 
 }
