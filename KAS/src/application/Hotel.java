0d0bab8fea18e6de44aaabbc6ece05176191d893
Administrator <Administrator@10.24.64.97>
2018-04-19 09:50:54 +0200

    Added associated til HotelTillaeg.

--- KAS/src/application/Hotel.java
+++ KAS/src/application/Hotel.java
@@ -1,10 +1,13 @@
 package application;
 
+import java.util.ArrayList;
+
 public class Hotel {
     private String navn;
     private String adresse;
     private double prisEnkelt;
     private double prisDobbelt;
+    private ArrayList<HotelTillaeg> hotelTillaeg = new ArrayList<>();
 
     public Hotel(String navn, String adresse, double prisEnkelt, double prisDobbelt) {
         this.navn = navn;
@@ -47,9 +50,21 @@ public class Hotel {
         this.prisDobbelt = prisDobbelt;
     }
 
+    public void addHotelTillaeg(HotelTillaeg tillaeg) {
+        hotelTillaeg.add(tillaeg);
+    }
+
+    public void removeHotelTillaeg(HotelTillaeg tillaeg) {
+        hotelTillaeg.remove(tillaeg);
+    }
+
+    public ArrayList<HotelTillaeg> getHotelTillaeg() {
+        return new ArrayList<>(hotelTillaeg);
+    }
+
     @Override
     public String toString() {
-        return navn + "(" + prisEnkelt + "/" + prisDobbelt + ")";
+        return navn + " (" + prisEnkelt + "/" + prisDobbelt + ")";
     }
 
 }
