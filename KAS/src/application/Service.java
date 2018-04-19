3cd0a70b18d8feaa53d5a7487fe02e956c3a4cd8
Administrator <Administrator@10.24.64.97>
2018-04-19 09:34:06 +0200

    Added handling of hotels.

--- KAS/src/application/Service.java
+++ KAS/src/application/Service.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Service {
 
     private static ArrayList<Konference> konferencer = new ArrayList<>();
+    private static ArrayList<Hotel> hoteller = new ArrayList<>();
 
     public static void addKonference(Konference konference) {
         konferencer.add(konference);
@@ -12,6 +13,14 @@ public class Service {
 
     public static ArrayList<Konference> getKonferencer() {
         return new ArrayList<>(konferencer);
+    }
+
+    public static void addHotel(Hotel hotel) {
+        hoteller.add(hotel);
+    }
+
+    public static ArrayList<Hotel> getHotels() {
+        return new ArrayList<>(hoteller);
     }
 
 }
