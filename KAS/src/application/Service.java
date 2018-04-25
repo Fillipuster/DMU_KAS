d22f775ee6fdb98a64a9fd216e937faf281e5638
Admin <Admin@10.24.2.71>
2018-04-25 10:51:39 +0200

    Changed all instances of LocalDateTime to LocalDate.

--- KAS/src/application/Service.java
+++ KAS/src/application/Service.java
@@ -1,59 +1,58 @@
 package application;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Service {
 
-    private static ArrayList<Konference> konferencer = new ArrayList<>();
-    private static ArrayList<Hotel> hoteller = new ArrayList<>();
+	private static ArrayList<Konference> konferencer = new ArrayList<>();
+	private static ArrayList<Hotel> hoteller = new ArrayList<>();
 
-    public static void addKonference(Konference konference) {
-        konferencer.add(konference);
-    }
+	public static void addKonference(Konference konference) {
+		konferencer.add(konference);
+	}
 
-    public static ArrayList<Konference> getKonferencer() {
-        return new ArrayList<>(konferencer);
-    }
+	public static ArrayList<Konference> getKonferencer() {
+		return new ArrayList<>(konferencer);
+	}
 
-    public static void addHotel(Hotel hotel) {
-        hoteller.add(hotel);
-    }
+	public static void addHotel(Hotel hotel) {
+		hoteller.add(hotel);
+	}
 
-    public static ArrayList<Hotel> getHotels() {
-        return new ArrayList<>(hoteller);
-    }
+	public static ArrayList<Hotel> getHotels() {
+		return new ArrayList<>(hoteller);
+	}
 
-    public static void createTilmelding(Konference konference, LocalDate ankomstDato, LocalDate afrejseDato,
-            Person deltager, Person ledsager) {
-        Tilmelding t = new Tilmelding(konference, ankomstDato, afrejseDato, deltager, ledsager);
-    }
+	public static void createTilmelding(Konference konference, LocalDate ankomstDato, LocalDate afrejseDato,
+			Person deltager, Person ledsager) {
+		Tilmelding t = new Tilmelding(konference, ankomstDato, afrejseDato, deltager, ledsager);
+	}
 
-    public static Konference createKonference(String navn, String adresse, LocalDate fraDato, LocalDate tilDato,
-            String beskrivelse) {
-        Konference k = new Konference(navn, adresse, fraDato, tilDato, beskrivelse);
-        addKonference(k);
+	public static Konference createKonference(String navn, String adresse, LocalDate fraDato, LocalDate tilDato,
+			String beskrivelse) {
+		Konference k = new Konference(navn, adresse, fraDato, tilDato, beskrivelse);
+		addKonference(k);
 
-        return k;
-    }
+		return k;
+	}
 
-    public static void createPerson(String fornavn, String efternavn, String adresse, String telefonNr) {
-        Person p = new Person(fornavn, efternavn, adresse, telefonNr);
-    }
+	public static void createPerson(String fornavn, String efternavn, String adresse, String telefonNr) {
+		Person p = new Person(fornavn, efternavn, adresse, telefonNr);
+	}
 
-    public static Udflugt createUdflugt(Konference konference, String navn, String beskrivelse, LocalDateTime fraTid,
-            LocalDateTime tilTid, double pris, boolean frokost) {
-        Udflugt u = new Udflugt(navn, beskrivelse, fraTid, tilTid, pris, frokost);
-        konference.addUdflugt(u);
+	public static Udflugt createUdflugt(Konference konference, String navn, String beskrivelse, LocalDate fraTid,
+			LocalDate tilTid, double pris, boolean frokost) {
+		Udflugt u = new Udflugt(navn, beskrivelse, fraTid, tilTid, pris, frokost);
+		konference.addUdflugt(u);
 
-        return u;
-    }
+		return u;
+	}
 
-    public static HotelTillaeg createHotelTillaeg(Hotel hotel, String navn, double pris) {
-        HotelTillaeg ht = new HotelTillaeg(navn, pris);
-        hotel.addHotelTillaeg(ht);
+	public static HotelTillaeg createHotelTillaeg(Hotel hotel, String navn, double pris) {
+		HotelTillaeg ht = new HotelTillaeg(navn, pris);
+		hotel.addHotelTillaeg(ht);
 
-        return ht;
-    }
+		return ht;
+	}
 }
--- KAS/src/gui/MainApp.java
+++ KAS/src/gui/MainApp.java
@@ -1,134 +1,135 @@
 package gui;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import application.Hotel;
 import application.HotelTillaeg;
 import application.Konference;
 import application.Service;
 import javafx.application.Application;
-import javafx.geometry.*;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
-    private static void createTestData() {
-        Konference k = Service.createKonference("Baconferencen", "Ham Road 22", LocalDate.now().plusDays(3),
-                LocalDate.now().plusDays(5), "Det handler om bacon, drenge.");
+	private static void createTestData() {
+		Konference k = Service.createKonference("Baconferencen", "Ham Road 22", LocalDate.now().plusDays(3),
+				LocalDate.now().plusDays(5), "Det handler om bacon, drenge.");
 
-        Service.createUdflugt(k, "U1", "bla", LocalDateTime.now(), LocalDateTime.now(), 120, true);
-        Service.createUdflugt(k, "U2", "bllla", LocalDateTime.now(), LocalDateTime.now(), 202, false);
+		Service.createUdflugt(k, "U1", "bla", LocalDate.now(), LocalDate.now(), 120, true);
+		Service.createUdflugt(k, "U2", "bllla", LocalDate.now(), LocalDate.now(), 202, false);
 
-        HotelTillaeg ht1 = new HotelTillaeg("Wifi", 50);
-        HotelTillaeg ht2 = new HotelTillaeg("Morgenmad", 400);
-        Hotel h = new Hotel("Testotel", "Whatever", 100, 200);
-        h.addHotelTillaeg(ht1);
-        h.addHotelTillaeg(ht2);
+		HotelTillaeg ht1 = new HotelTillaeg("Wifi", 50);
+		HotelTillaeg ht2 = new HotelTillaeg("Morgenmad", 400);
+		Hotel h = new Hotel("Testotel", "Whatever", 100, 200);
+		h.addHotelTillaeg(ht1);
+		h.addHotelTillaeg(ht2);
 
-        // Person p1 = new Person("Jonas", "Berg", "Whatever", "288713");
-        // Person p2 = new Person("Daniel", "Præstegaard", "Gah", "923884");
-        //
-        // Tilmelding t = new Tilmelding(_tk1, LocalDate.now(), LocalDate.now(), p1);
-        // t.setLedsager(p2);
-        // t.addUdflugt(u2);
-        // t.setHotel(h);
-        // t.addTillaeg(ht);
-        //
-        // System.out.println(t.totalPrice());
-        // System.out.println(200 + 9001 + 202);
-    }
+		// Person p1 = new Person("Jonas", "Berg", "Whatever", "288713");
+		// Person p2 = new Person("Daniel", "Præstegaard", "Gah", "923884");
+		//
+		// Tilmelding t = new Tilmelding(_tk1, LocalDate.now(), LocalDate.now(), p1);
+		// t.setLedsager(p2);
+		// t.addUdflugt(u2);
+		// t.setHotel(h);
+		// t.addTillaeg(ht);
+		//
+		// System.out.println(t.totalPrice());
+		// System.out.println(200 + 9001 + 202);
+	}
 
-    public static void main(String[] args) {
-        createTestData();
-        Application.launch(args);
-    }
+	public static void main(String[] args) {
+		createTestData();
+		Application.launch(args);
+	}
 
-    @Override
-    public void start(Stage stage) {
-        stage.setTitle("Konference Administrations System");
-        GridPane pane = new GridPane();
-        initContent(pane);
+	@Override
+	public void start(Stage stage) {
+		stage.setTitle("Konference Administrations System");
+		GridPane pane = new GridPane();
+		initContent(pane);
 
-        viewConference = new ViewConferenceWindow("Konferencevisning", stage);
-        createConference = new CreateConferenceWindow("Opret Konference", stage);
-        joinconference = new JoinConferenceWindow("Tilmeld Konference", stage);
+		viewConference = new ViewConferenceWindow("Konferencevisning", stage);
+		createConference = new CreateConferenceWindow("Opret Konference", stage);
+		joinconference = new JoinConferenceWindow("Tilmeld Konference", stage);
 
-        Scene scene = new Scene(pane);
-        stage.setScene(scene);
-        stage.show();
-    }
+		Scene scene = new Scene(pane);
+		stage.setScene(scene);
+		stage.show();
+	}
 
-    private final Controller controller = new Controller();
-    private ViewConferenceWindow viewConference;
-    private CreateConferenceWindow createConference;
-    private JoinConferenceWindow joinconference;
-    private ComboBox<Konference> cbKonferencer;
-    private Button btnJoin, btnView, btnCreate;
+	private final Controller controller = new Controller();
+	private ViewConferenceWindow viewConference;
+	private CreateConferenceWindow createConference;
+	private JoinConferenceWindow joinconference;
+	private ComboBox<Konference> cbKonferencer;
+	private Button btnJoin, btnView, btnCreate;
 
-    private void initContent(GridPane pane) {
-        pane.setGridLinesVisible(false);
-        pane.setPadding(new Insets(20));
-        pane.setHgap(10);
-        pane.setVgap(10);
+	private void initContent(GridPane pane) {
+		pane.setGridLinesVisible(false);
+		pane.setPadding(new Insets(20));
+		pane.setHgap(10);
+		pane.setVgap(10);
 
-        Label lblTitle = new Label("Vælg konference:");
-        pane.add(lblTitle, 0, 0);
+		Label lblTitle = new Label("Vælg konference:");
+		pane.add(lblTitle, 0, 0);
 
-        cbKonferencer = new ComboBox<>();
-        cbKonferencer.setMinWidth(308);
-        pane.add(cbKonferencer, 0, 1, 2, 1);
-        controller.updateKonferencerListe();
-        cbKonferencer.setOnAction(event -> controller.cbKonferencerAction());
+		cbKonferencer = new ComboBox<>();
+		cbKonferencer.setMinWidth(308);
+		pane.add(cbKonferencer, 0, 1, 2, 1);
+		controller.updateKonferencerListe();
+		cbKonferencer.setOnAction(event -> controller.cbKonferencerAction());
 
-        btnJoin = new Button("Tilmeld Konference");
-        pane.add(btnJoin, 3, 1);
-        btnJoin.setOnAction(event -> controller.btnJoinAction());
-        btnJoin.setDisable(true);
+		btnJoin = new Button("Tilmeld Konference");
+		pane.add(btnJoin, 3, 1);
+		btnJoin.setOnAction(event -> controller.btnJoinAction());
+		btnJoin.setDisable(true);
 
-        btnView = new Button("Konferencedetaljer");
-        pane.add(btnView, 0, 3);
-        btnView.setOnAction(event -> controller.btnViewAction());
-        btnView.setDisable(true);
+		btnView = new Button("Konferencedetaljer");
+		pane.add(btnView, 0, 3);
+		btnView.setOnAction(event -> controller.btnViewAction());
+		btnView.setDisable(true);
 
-        btnCreate = new Button("Opret Konference");
-        pane.add(btnCreate, 1, 3);
-        btnCreate.setOnAction(event -> controller.btnCreateAction());
+		btnCreate = new Button("Opret Konference");
+		pane.add(btnCreate, 1, 3);
+		btnCreate.setOnAction(event -> controller.btnCreateAction());
 
-        Label lblCopyright = new Label("© Berg, Valentin & Priestyard 2018");
-        pane.add(lblCopyright, 0, 4);
-    }
+		Label lblCopyright = new Label("© Berg, Valentin & Priestyard 2018");
+		pane.add(lblCopyright, 0, 4);
+	}
 
-    private class Controller {
-        public void btnCreateAction() {
-            createConference.showAndWait();
-            if (createConference.konference != null) {
-                System.out.println("Konference oprettet!");
-                updateKonferencerListe();
-            }
-        }
+	private class Controller {
+		public void btnCreateAction() {
+			createConference.showAndWait();
+			if (createConference.konference != null) {
+				System.out.println("Konference oprettet!");
+				updateKonferencerListe();
+			}
+		}
 
-        public void btnViewAction() {
-            viewConference.setKonference(cbKonferencer.getSelectionModel().getSelectedItem());
-            viewConference.showAndWait();
-        }
+		public void btnViewAction() {
+			viewConference.setKonference(cbKonferencer.getSelectionModel().getSelectedItem());
+			viewConference.showAndWait();
+		}
 
-        public void btnJoinAction() {
-            joinconference.konference = cbKonferencer.getSelectionModel().getSelectedItem();
-            joinconference.showAndWait();
-        }
+		public void btnJoinAction() {
+			joinconference.konference = cbKonferencer.getSelectionModel().getSelectedItem();
+			joinconference.showAndWait();
+		}
 
-        private void updateKonferencerListe() {
-            cbKonferencer.getItems().setAll(Service.getKonferencer());
-        }
+		private void updateKonferencerListe() {
+			cbKonferencer.getItems().setAll(Service.getKonferencer());
+		}
 
-        private void cbKonferencerAction() {
-            btnJoin.setDisable(false);
-            btnView.setDisable(false);
-        }
-    }
+		private void cbKonferencerAction() {
+			btnJoin.setDisable(false);
+			btnView.setDisable(false);
+		}
+	}
 
 }
